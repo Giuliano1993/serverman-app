6e2682ad00dc8645454cd8a2c0cd2acb33fa3d08
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2024-10-20 11:41:08 +0200

    First UI approach

--- src/components/ServerList.vue
+++ src/components/ServerList.vue
@@ -1,19 +1,43 @@
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue'
-import { listSites } from '../utils/server.ts'
 import Netlify from '../providers/netlify.ts';
-defineProps([])
+import { NetlifySite, Provider, Server } from '../types/types.d.ts';
+import ServerListElement from './ServerListElement.vue'
+import Vercel from '../providers/vercel.ts';
+const props = defineProps<{
+  type: Provider,
+}>();
+const sites = defineModel()
 defineEmits([])
 
 onMounted( async ()=>{
-  const sites=  await Netlify.listSites();
-  console.log(sites)
+  let loadedSites: Server[] = [];
+  switch (props.type.toLowerCase()){
+    case Provider.NETLIFY:
+      if(Netlify.verifyNetlifyConfig){
+        loadedSites = await Netlify.listSites().then((res : any)=>res.map(Netlify.convertServerToGeneric))
+        
+      }
+      break
+    case Provider.VERCEL:
+        loadedSites = await Vercel.projectList().then((res: any) => res.map(Vercel.convertServerToGeneric));
+        
+      break
+    case Provider.DIGITALOCEAN:
+      break
+  }
+  console.log(loadedSites)
+  sites.value = loadedSites
+  
 })
 
 </script>
 
 <template>
-  Hello ServerList
+  <div>
+    <h1> {{ props.type }} servers</h1>
+    <ServerListElement v-for="site in sites" :site="site" :key="site.id"></ServerListElement>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/ServerListElement.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import { Server } from '../types/types.d.ts'
+import { preview } from 'vite';
+const props = defineProps<{
+  site: Server
+}>()
+defineEmits([])
+
+
+</script>
+
+<template>
+  <Card style="width: 25rem; overflow: hidden">
+      <template #header>
+          <img alt="user header" :src="site.preview_image_url ?? '/src/assets/server.png'"  width="100px"/>
+      </template>
+      <template #title>{{site.name}}</template>
+      <template #footer>
+          <div class="flex gap-4 mt-1">
+              <Button label="Cancel" severity="secondary" outlined class="w-full" />
+              <Button label="Save" class="w-full" />
+          </div>
+      </template>
+  </Card>
+</template>
+
+<style scoped>
+</style>
