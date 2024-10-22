cb8efe3a0a8865d8df1eaf9ea4fdffcbd7dccac6
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2024-10-22 08:04:15 +0200

    init stronghold

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,10 +7,20 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_stronghold::Builder::new(|pass| todo!()).build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
+            .setup(|app| {
+                let salt_path = app
+                    .path()
+                    .app_home_dir()
+                    .expect("could not resolve app local data path")
+                    .join("salt.txt");
+                app.handle().plugin(tauri_plugin_stronghold::Builder::with_argon2(&salt_path).build())?;
+                Ok(())
+            })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
